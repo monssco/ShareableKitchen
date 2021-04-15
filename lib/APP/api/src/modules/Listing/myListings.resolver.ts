3b832e454e9096b368e6ad002b5fd30d32af78ac
Sonny <munibrhmn@gmail.com>
2021-04-14 23:59:39 -0600

    added resolver for editing listings

--- lib/APP/api/src/modules/Listing/myListings.resolver.ts
+++ lib/APP/api/src/modules/Listing/myListings.resolver.ts
@@ -1,8 +1,41 @@
 import { Listing } from "../../entities/Listing/Listing"
 import { User } from "../../entities/User/User"
 import { MyContext } from "../../types"
-import { Ctx, Field, Query, Root } from "type-graphql"
+import { Arg, Ctx, Field, FieldResolver, InputType, Int, Mutation, Query, Resolver, Root } from "type-graphql"
 import { Availability } from "../../entities/Availability"
+import { PropertyFeatures } from "../../entities/Enums/PropertyFeatures.enum"
+import { PropertyType } from "../../entities/Enums/PropertyType.enum"
+
+/**
+ * TODO: Add support for location editing and editing pictures.
+ */
+@InputType()
+class EditMyListingInput implements Partial<Listing> {
+
+    @Field()
+    id!: string
+
+    @Field({nullable: true})
+    title?: string
+
+    @Field({nullable: true})
+    description?: string
+
+    @Field(()=> Availability, ({nullable: true}))
+    availability?: Availability
+
+    @Field(()=> Int, {nullable: true})
+    price?: number
+
+    @Field(()=> Int, {nullable: true})
+    sqFtArea?: number
+
+    @Field(()=> [PropertyFeatures], {nullable: true})
+    features?: PropertyFeatures[]
+
+    @Field(()=> PropertyType, {nullable: true})
+    propertyType?:PropertyType
+}
 
 export class MyListingResolver {
 
@@ -21,7 +54,75 @@ export class MyListingResolver {
         return listings
     }
 
-    @Field(()=>Availability)
+    /**
+     * Things you can edit:
+     * Title
+     * Description
+     * Availability
+     * Photos
+     * Location
+     * Price
+     * SqFt Area
+     * Features
+     * PropertyType
+     * @param param0 listing
+     */
+    @Mutation(() => Listing)
+    async editMyListing(
+        @Arg("input", {description:"Required input for editing your own listing"}) input: EditMyListingInput,
+        @Ctx() {em, user}: MyContext
+    ) {
+        const dbUser = await em.findOneOrFail(User, {id: user?.sub})
+        const listing = await em.findOneOrFail(Listing, {id: input.id})
+
+        if (listing.author != dbUser) {
+            throw Error('Not your listing >:(')
+        }
+
+        if (input.title) {
+            listing.title = input.title
+        }
+
+        if (input.description) {
+            listing.description = input.description
+        }
+
+        if (input.availability) {
+            listing.availability = input.availability
+        }
+
+        if (input.price) {
+            listing.price = input.price
+        }
+
+        if (input.sqFtArea) {
+            listing.sqFtArea = input.sqFtArea
+        }
+
+        if (input.features) {
+            listing.features = input.features
+        }
+
+        if (input.propertyType) {
+            listing.propertyType = input.propertyType
+        }
+
+        await em.persistAndFlush(listing)
+
+
+        return listing
+
+    }
+
+    
+}
+
+/**
+ * Field resolver for availability attribute on the Listing class.
+ */
+@Resolver(() => Listing)
+export class AvailabilityResolver {
+    @FieldResolver(()=>Availability)
     async availability(
         @Root() listing: Listing,
         @Ctx() {em}: MyContext
